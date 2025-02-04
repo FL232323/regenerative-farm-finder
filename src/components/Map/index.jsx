2590c7b8af041acfd16b8dd7e3385b42fabecee1
FL232323 <Fosterleboeuf23@gmail.com>
2025-02-04 15:23:27 -0500

    Add Next.js map implementation with Leaflet

---
+++ src/components/Map/index.jsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+
+const Map = () => {
+  // Default to US center coordinates
+  const defaultCenter = [39.8283, -98.5795];
+  
+  return (
+    <MapContainer
+      center={defaultCenter}
+      zoom={4}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
+};
+
+export default Map;
