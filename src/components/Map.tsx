8aeb8b37ac6fc919c633bbd0be489d7d9df95286
FL232323 <Fosterleboeuf23@gmail.com>
2025-02-04 19:49:10 -0500

    Update Map component with proper Leaflet imports and styling

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -42,60 +42,62 @@ function MapController({ center, zoom }: { center: [number, number]; zoom: numbe
 
 export default function Map({ farms, center, zoom }: MapProps) {
   return (
-    <MapContainer 
-      center={center}
-      zoom={zoom}
-      scrollWheelZoom={true}
-      className="h-[600px] w-full"
-    >
-      <MapController center={center} zoom={zoom} />
-      
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      
-      {farms.map((farm) => (
-        <Marker 
-          key={farm._id} 
-          position={[farm.location.coordinates[1], farm.location.coordinates[0]]}
-        >
-          <Popup className="min-w-[200px]">
-            <div className="py-2">
-              <h3 className="font-bold text-lg mb-1">{farm.name}</h3>
-              
-              <div className="text-sm text-gray-600 mb-2">
-                {farm.businessType.join(', ')}
-              </div>
-              
-              {farm.address && (
-                <div className="text-sm mb-2">
-                  {farm.address.street && <div>{farm.address.street}</div>}
-                  <div>
-                    {[farm.address.city, farm.address.state, farm.address.zipCode]
-                      .filter(Boolean)
-                      .join(', ')}
+    <div className="h-[600px] w-full rounded-lg overflow-hidden shadow-lg">
+      <MapContainer 
+        center={center}
+        zoom={zoom}
+        scrollWheelZoom={true}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <MapController center={center} zoom={zoom} />
+        
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        
+        {farms.map((farm) => (
+          <Marker 
+            key={farm._id} 
+            position={[farm.location.coordinates[1], farm.location.coordinates[0]]}
+          >
+            <Popup className="min-w-[200px]">
+              <div className="py-2">
+                <h3 className="font-bold text-lg mb-1">{farm.name}</h3>
+                
+                <div className="text-sm text-gray-600 mb-2">
+                  {farm.businessType.join(', ')}
+                </div>
+                
+                {farm.address && (
+                  <div className="text-sm mb-2">
+                    {farm.address.street && <div>{farm.address.street}</div>}
+                    <div>
+                      {[farm.address.city, farm.address.state, farm.address.zipCode]
+                        .filter(Boolean)
+                        .join(', ')}
+                    </div>
                   </div>
-                </div>
-              )}
-              
-              {typeof farm.distance === 'number' && (
-                <div className="text-sm text-gray-600">
-                  {farm.distance} miles away
-                </div>
-              )}
-              
-              {farm.description && (
-                <div className="mt-2 text-sm">
-                  {farm.description.length > 150 
-                    ? `${farm.description.substring(0, 150)}...` 
-                    : farm.description}
-                </div>
-              )}
-            </div>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+                )}
+                
+                {typeof farm.distance === 'number' && (
+                  <div className="text-sm text-gray-600">
+                    {farm.distance} miles away
+                  </div>
+                )}
+                
+                {farm.description && (
+                  <div className="mt-2 text-sm">
+                    {farm.description.length > 150 
+                      ? `${farm.description.substring(0, 150)}...` 
+                      : farm.description}
+                  </div>
+                )}
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 }
